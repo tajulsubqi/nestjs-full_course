0043112521c3fffa74e040c1a0b0dc661fb2221a
Tajul <subqitajul7@gmail.com>
2025-02-05 17:36:36 +0700

    feat: added create employees

---
+++ src/employees/employees.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { EmployeesService } from './employees.service';
+
+@Controller('employees')
+export class EmployeesController {
+  constructor(private readonly employeesService: EmployeesService) {}
+
+  @Post()
+  create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
+    return this.employeesService.create(createEmployeeDto);
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.employeesService.findAll();
+  // }
+
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
+    return this.employeesService.findAll(role);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.employeesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput,
+  ) {
+    return this.employeesService.update(+id, updateEmployeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.employeesService.remove(+id);
+  }
+}
